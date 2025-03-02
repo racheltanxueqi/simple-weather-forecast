1aa3168cc68f2a36d0c35fac207e5c4b558e754b
Rachel <racheltanxueqi@gmail.com>
2025-03-03 00:04:56 +0800

    add responsiveness

--- src/components/HistoryCard.js
+++ src/components/HistoryCard.js
@@ -7,9 +7,11 @@ const HistoryCard = ({history,handleSearchClick, removeHistoryItem}) => {
 
     const renderHistoryRow = (item, index) => {
         return (<div className="history-row">
-            <p>{item.city}, {item.country}</p>
-            <div className="history-row-actions">
+            <div className="history-row-text">
+                <p className="history-city">{item.city}, {item.country}</p>
                 <p style={{marginRight: '2px'}}>{formatDateTime(item.createdAt)}</p>
+            </div>
+            <div className="history-row-actions">
                 <IconButton type="button" alt="search-icon" Icon={FaSearch} onClick={() => handleSearchClick({city: item.city, country: item.country, createdAt: new Date()})}/>
                 <IconButton type="button" alt="delete-icon" Icon={FaTrash} onClick={() => removeHistoryItem(index)}/>
             </div>
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -19,15 +19,17 @@ const WeatherCard = ({ weather }) => {
     <div>
       {renderWeatherIcon()}
       <div className="weather-main">
-        <p>Today's Weather</p>
-        <h1>{Math.round(weather.main.temp)}&deg;</h1>
-        <p>H: {Math.round(weather.main.temp_max)}&deg; L: {Math.round(weather.main.temp_min)}&deg;</p>
-      <div className="weather-details">
-        <p className="location">{weather.name}, {weather.sys.country}</p>
-        <p>{formatDateTime(new Date())}</p>
-        <p>Humidity: {weather.main.humidity}%</p>
-        <p>{weather.weather[0].description}</p>
-      </div>
+        <div style={{display: 'flex', flexDirection: "column"}}>
+          <p>Today's Weather</p>
+          <h1>{Math.round(weather.main.temp)}&deg;</h1>
+          <p>H: {Math.round(weather.main.temp_max)}&deg; L: {Math.round(weather.main.temp_min)}&deg;</p>
+          <p className="location">{weather.name}, {weather.sys.country}</p>
+        </div>
+        <div className="weather-specifics">
+          <p>{formatDateTime(new Date())}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>{weather.weather[0].description}</p>
+        </div>
       </div>
     </div>
   );
